e8f76971b66af6e0726ed1483ed3ba0484d68e96
Christopher Hoch <chris@hpc.at>
2025-02-21 15:15:15 +0100

    added configuration.h

---
+++ software/src/configuration.h
@@ -0,0 +1,51 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+// optional, can be configured over hotspot
+//#define WIFI_SSID "CHANGEME"
+//#define WIFI_PASSWORD "CHANGEME"
+//#define WIFI_HOST "Virtual Vitotrol"
+#define REBOOT_TIMEOUT (1000*60*15)
+
+
+
+// if you want to use MQTT, uncomment these lines and replace them accordingly
+
+// optional, can be configured over hotspot
+//#define MQTT_HOST "CHANGEME"
+//#define MQTT_USER "CHANGEME"
+//#define MQTT_PASS "CHANGEME"
+
+// VITOTROL CONFIGURATION
+#define DEVICE_CLASS 0x11
+
+// vitotrol 200
+#define DEVICE_ID 0x34
+#define DEVICE_SN1 0x00
+#define DEVICE_SN2 0x05
+#define DEVICE_SLOT 0x01
+
+// vitotrol 300
+//#define DEVICE_ID 0x38
+//#define DEVICE_SN1 0x00
+//#define DEVICE_SN2 0x11
+//#define DEVICE_SLOT 0x02
+
+#if defined(ESP32)
+#define MODBUS_BAUD SERIAL_8E1
+#define ModbusSerial Serial1
+#define DebugSerial Serial
+#define MODBUS_RX 17
+#define MODBUS_TX 16
+#elif defined(ESP8266)
+#include <SoftwareSerial.h>
+extern SoftwareSerial softwareSerial;
+#define MODBUS_BAUD SWSERIAL_8E1
+#define ModbusSerial softwareSerial
+#define DebugSerial Serial
+#define MODBUS_RX D4
+#define MODBUS_TX D5
+#endif
+#define DEBUG false
+
+#endif // CONFIGURATION_H
